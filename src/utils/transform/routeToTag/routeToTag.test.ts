d2904af65a9b5ee9dda89e7a33166772cbe1ea51
Martin Bobbio <martinbobbio1@gmail.com>
2023-11-23 00:38:47 -0300

    remove unused utils

--- src/utils/transform/routeToTag/routeToTag.test.ts
+++
@@ -1,9 +0,0 @@
-import routeToTag from './routeToTag';
-import { expect, test, describe } from 'vitest';
-
-describe('routeToTag', () => {
-  test('Should handle first letter uppercase correctly', () => {
-    const result = routeToTag('/my-path');
-    expect(result).toBe('MY PATH');
-  });
-});
--- src/utils/transform/routeToTag/routeToTag.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Function that parse a route like a title
- *
- * @param value for the parse
- * @return string
- */
-export default (value: string): string => {
-  return value.replaceAll('/', '');
-};
